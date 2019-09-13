2b918de286a80471b944944baebea2b69a9f0191
Ben Hale <bhale@pivotal.io>
2019-09-13 14:11:13 -0700

    merge-upstream-module
    Signed-off-by: Ben Hale <bhale@pivotal.io>

---
+++ merge-upstream-module.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+
+cd "$(dirname "${BASH_SOURCE[0]}")/../source"
+
+git remote add upstream ../upstream
+git fetch upstream --no-tags
+
+git \
+  -c user.name='Pivotal Java Experience Team' \
+  -c user.email='cfje@pivotal.io' \
+  merge \
+  --no-ff \
+  --log \
+  --no-edit \
+  $(cat ../upstream/.git/ref)
