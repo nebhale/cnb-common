24ab8f808394aefdd039663720942a5efc9dfa90
Ben Hale <bhale@pivotal.io>
2019-09-13 08:25:49 -0700

    update-module-dependencies
    Signed-off-by: Ben Hale <bhale@pivotal.io>

---
+++ update-module-dependencies.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+if [[ -d $PWD/go-module-cache && ! -d ${GOPATH}/pkg/mod ]]; then
+  mkdir -p ${GOPATH}/pkg
+  ln -s $PWD/go-module-cache ${GOPATH}/pkg/mod
+fi
+
+cd "$(dirname "${BASH_SOURCE[0]}")/../source"
+go get -u all
+go mod tidy
+
