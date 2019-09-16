85309b448c2273b4ea3825432eb461b40b141121
Ben Hale <bhale@pivotal.io>
2019-09-16 10:52:43 -0700

    Revert "Remove Module Cache from Update Module Dependencies"
    This reverts commit 060f0b42f459722efaa3495d6cbee9a332fd73d3.

--- update-module-dependencies.sh
+++ update-module-dependencies.sh
@@ -2,6 +2,11 @@
 
 set -euo pipefail
 
+if [[ -d $PWD/go-module-cache && ! -d ${GOPATH}/pkg/mod ]]; then
+  mkdir -p ${GOPATH}/pkg
+  ln -s $PWD/go-module-cache ${GOPATH}/pkg/mod
+fi
+
 cd "$(dirname "${BASH_SOURCE[0]}")/../source"
 
 GOPRIVATE=* go get -u all
